b0dfb1484ca61dc078a33bb8cd45f2d24a7c8c65
whirae <111400253+whirae@users.noreply.github.com>
2024-09-18 22:16:18 +0900

    Update adder.c

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -4,7 +4,44 @@
 /* $begin adder */
 #include "csapp.h"
 
-int main(void) {
+int main(void)
+{
+  char *buf, *p;
+  char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+  int n1 = 0, n2 = 0;
+
+  /* Extract the two arguments */
+  if ((buf = getenv("QUERY_STRING")) != NULL)
+  {
+    p = strchr(buf, '&');
+    *p = '\0';
+    strcpy(arg1, buf);
+    strcpy(arg2, p + 1);
+    n1 = atoi(strchr(arg1, '=') + 1);
+    n2 = atoi(strchr(arg2, '=') + 1);
+  }
+
+  /* Make the response body */
+  // content 인자에 html body 를 담는다.
+  sprintf(content, "QUERY_STRING=%s", buf);
+  sprintf(content, "Welcome to add.com: ");
+  sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+  sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>",
+          content, n1, n2, n1 + n2);
+  sprintf(content, "%sThanks for visiting!\r\n", content);
+
+  /* Generate the HTTP response */
+  printf("Connection: close\r\n");
+  printf("Content-length: %d\r\n", (int)strlen(content));
+  printf("Content-type: text/html\r\n\r\n");
+  // 여기까지 가 헤더
+
+  // 여기부터 바디 출력
+  printf("%s", content);
+  fflush(stdout);
+
   exit(0);
 }
 /* $end adder */
+
+// http://3.35.205.51:8000/cgi-bin/adder?arg1=10&arg2=30 로 확인
